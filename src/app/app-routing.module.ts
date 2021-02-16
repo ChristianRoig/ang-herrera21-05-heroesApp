cf85d9a567f07997abc640944249eac89ba8ba5b
ChristianRoig <ch.roig@gmail.com>
2021-02-16 19:01:37 -0300

    seccion 173

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
   {
     path:'auth',
     loadChildren: () => import('./auth/auth.module').then( m=> m.AuthModule )
+  },  
+  {
+    path:'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then( m=> m.HeroesModule )
   },
   {
     path:'404',
